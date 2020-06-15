aa125cb0d7a0ea8f949fba005816eada21a287e6
Vishalghyv <vishal38785@gmail.com>
2020-06-15 20:48:05 +0530

    Removing comments

--- commerce_fraud.post_update.php
+++ commerce_fraud.post_update.php
@@ -8,12 +8,12 @@
 /**
  * Remove primary key and add note field to commerce_fraud_fraud_score table.
  */
-// function commerce_fraud_update_7100() {
-//   $table = 'commerce_fraud_fraud_score';
-//   db_drop_primary_key($table);
-//   $spec = array(
-//     'type' => 'varchar',
-//     'length' => 255,
-//   );
-//   db_add_field($table, 'note', $spec);
-// }
+function commerce_fraud_update_7100() {
+  $table = 'commerce_fraud_fraud_score';
+  db_drop_primary_key($table);
+  $spec = array(
+    'type' => 'varchar',
+    'length' => 255,
+  );
+  db_add_field($table, 'note', $spec);
+}
