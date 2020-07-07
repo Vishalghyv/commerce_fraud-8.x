6608c449b4ef8156072cac635dd14e41198f7852
Vishalghyv <vishal38785@gmail.com>
2020-07-08 02:56:31 +0530

    new rules

--- tests/src/Functional/ResetOrderScoreTest.php
+++ tests/src/Functional/ResetOrderScoreTest.php
@@ -6,12 +6,14 @@ use Drupal\Tests\commerce_order\Functional\OrderBrowserTestBase;
 use Drupal\commerce_order\Entity\Order;
 use Drupal\commerce_order\Entity\OrderItem;
 use Drupal\Tests\commerce_order\FunctionalJavascript\OrderWebDriverTestBase;
+use Drupal\Tests\BrowserTestBase;
+
 /**
  * Tests the commerce_order entity forms.
  *
  * @group commerce
  */
-class ResetOrderScoreTest extends OrderWebDriverTestBase {
+class ResetOrderScoreTest extends OrderBrowserTestBase {
 
   /**
    * A test order.
@@ -45,9 +47,9 @@ class ResetOrderScoreTest extends OrderWebDriverTestBase {
   protected function setUp() {
     parent::setUp();
 
-    $this->installEntitySchema('rules');
-    $this->installConfig(['commerce_fraud']);
-    $this->installSchema('commerce_fraud', ['commerce_fraud_fraud_score']);
+    // $this->installEntitySchema('rules');
+    // $this->installConfig(['commerce_fraud']);
+    // $this->installSchema('commerce_fraud', ['commerce_fraud_fraud_score']);
 
     $order_item = $this->createEntity('commerce_order_item', [
       'type' => 'default',
@@ -71,10 +73,11 @@ class ResetOrderScoreTest extends OrderWebDriverTestBase {
    * Tests deleting an order programaticaly and through the UI.
    */
   public function testResetOrder() {
-    $this->drupalGet($this->order->toUrl('reset-fraud-score-form'));
-    // $this->assertSession()->pageTextContains(t("Do you want to reset order fraud score order -id: @id?", ['@id' => $this->order->id()]));
-    // $this->assertSession()->pageTextContains(t("Reset this order's fraud score to 0"));
-    $this->submitForm($values, 'Reset Fraud Score');
+    // $this->drupalGet($this->order->toUrl('reset-fraud-score-form'));
+    $this->drupalGet('/admin/commerce/orders/'.$this->order->id().'/reset_fraud');
+    $this->assertSession()->pageTextContains(t("Do you want to reset order fraud score order -id: @id?", ['@id' => $this->order->id()]));
+    $this->assertSession()->pageTextContains(t("Reset this order's fraud score to 0"));
+    $this->submitForm([], 'Reset Fraud Score');
     $collection_url = $this->order->toUrl('collection', ['absolute' => TRUE]);
     $this->assertSession()->addressEquals($collection_url);
     $this->assertSession()->pageTextContains(t('The order has been reseted.'));
---
+++ tests/src/Kernel/Plugin/Commerce/FraudRule/ProductAttributeFraudRuleTest.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Drupal\Tests\commerce_fraud\Kernel\Plugin\Commerce\FraudRule;
+
+use Drupal\commerce_order\Entity\Order;
+use Drupal\commerce_order\Entity\OrderItem;
+use Drupal\commerce_price\Calculator;
+use Drupal\commerce_price\Price;
+use Drupal\commerce_product\Entity\Product;
+use Drupal\commerce_product\Entity\ProductType;
+use Drupal\commerce_product\Entity\ProductVariation;
+use Drupal\commerce_fraud\Entity\Rules;
+use Drupal\Tests\commerce_order\Kernel\OrderKernelTestBase;
+
+/**
+ * Tests actions source plugin.
+ *
+ * @coversDefaultClass \Drupal\commerce_fraud\Plugin\Commerce\FraudRule\ProductAttributeFraudRule
+ * @group commerce
+ */
+class ProductAttributeFraudRuleTest extends OrderKernelTestBase {
+
+  /**
+   * The test order.
+   *
+   * @var \Drupal\commerce_order\Entity\OrderInterface
+   */
+  protected $order;
+
+  /**
+   * The test rule.
+   *
+   * @var \Drupal\commerce_fraud\Entity\RulesInterface
+   */
+  protected $rule;
+
+  /**
+   * The order item storage.
+   *
+   * @var \Drupal\commerce_order\OrderItemStorageInterface
+   */
+  protected $orderItemStorage;
+
+  /**
+   * The variation to test against.
+   *
+   * @var \Drupal\commerce_product\Entity\ProductVariation
+   */
+  protected $variations = [];
+
+  protected $product_one;
+  protected $product_two;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'commerce_fraud',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installEntitySchema('rules');
+    $this->installConfig(['commerce_fraud']);
+    $this->installSchema('commerce_fraud', ['commerce_fraud_fraud_score']);
+
+    $this->orderItemStorage = $this->container->get('entity_type.manager')->getStorage('commerce_order_item');
+
+    $this->order = Order::create([
+      'type' => 'default',
+      'state' => 'completed',
+      'mail' => 'test@example.com',
+      'ip_address' => '127.0.0.1',
+      'order_number' => '6',
+      'uid' => $this->createUser(),
+      'store_id' => $this->store,
+      'order_items' => [],
+    ]);
+
+    $this->variations[0] = ProductVariation::create([
+        'type' => 'default',
+        'sku' => 'jkfd',
+        'price' => [
+          'number' => 999,
+          'currency_code' => 'USD',
+        ],
+      ]);
+    $this->variations[0]->save();
+
+    $this->variations[1] = ProductVariation::create([
+        'type' => 'default',
+        'sku' => 'sdf',
+        'price' => [
+          'number' => 929,
+          'currency_code' => 'USD',
+        ],
+      ]);
+    $this->variations[1]->save();
+
+    $this->product_one = Product::create([
+      'type' => 'default',
+      'title' => 'Example 1',
+      'stores' => [$this->store],
+      'variations' => [$this->variations[0]],
+    ]);
+    $this->product_one->save();
+
+    $this->product_two = Product::create([
+      'type' => 'default',
+      'title' => 'Example 2',
+      'stores' => [$this->store],
+      'variations' => [$this->variations[1]],
+    ]);
+    $this->product_two->save();
+    // var_dump($this->product_one->uuid());
+    // var_dump($this->product_two->uuid());
+    // $this->variations[0] = $this->reloadEntity($this->variations[0]);
+    // $this->variations[1] = $this->reloadEntity($this->variations[1]);
+
+    $this->rule = Rules::create([
+      'id' => 'example',
+      'label' => 'Product Attribute',
+      'status' => TRUE,
+      'plugin' => 'product_attribute',
+      'product_conditions' => [
+            [
+              'plugin' => 'order_item_product',
+              'configuration' => [
+                'products' => [
+                  ['product' => $this->product_one->uuid()],
+                ],
+              ],
+            ],
+          ],
+      'counter' => 9,
+    ]);
+
+    $this->rule->save();
+  }
+
+  /**
+   * Tests the non-applicable use case.
+   *
+   * @covers ::apply
+   */
+  public function testNotApplicableRule() {
+    $order_item = $this->orderItemStorage->createFromPurchasableEntity($this->variations[1]);
+    $order_item->save();
+    // var_dump($order_item->uuid());
+    $this->order->addItem($order_item);
+    $this->order->save();
+    $this->assertEquals(FALSE, $this->rule->getPlugin()->apply($this->order));
+  }
+
+  /**
+   * Tests the applicable use case.
+   *
+   * @covers ::apply
+   */
+  public function testApplicableRule() {
+    $order_item = $this->orderItemStorage->createFromPurchasableEntity($this->variations[0]);
+    $order_item->save();
+    $this->order->addItem($order_item);
+    $this->order->save();
+    $this->assertEquals(TRUE, $this->rule->getPlugin()->apply($this->order));
+  }
+
+}
