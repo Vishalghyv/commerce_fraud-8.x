9b46085c8d2343b831d5c3fccba9dadebd8f0e35
Vishalghyv <vishal38785@gmail.com>
2020-07-17 21:32:44 +0530

    Machine learnaing

--- src/Form/CommerceFraudSettingsForm.php
+++ src/Form/CommerceFraudSettingsForm.php
@@ -4,6 +4,8 @@ namespace Drupal\commerce_fraud\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\NestedArray;
 
 /**
  * Configure commerce fraud settings for this site.
@@ -67,8 +69,99 @@ class CommerceFraudSettingsForm extends ConfigFormBase {
       '#required' => TRUE,
       '#description' => t('If an order is blocklisted its details will be sent to this email'),
     ];
+    $enable = $form_state->getValue('api_status');
+
+    $enable = $enable ?: (\Drupal::state()->get('commerce_fraud_aws_model', FALSE) ? 1 : 0);
+
+    $wrapper_id = Html::getUniqueId('commerce-fraud-aws-model');
+    $form['commerce_fraud_aws_model'] = [
+      '#title' => $this
+        ->t('Activate this add you AWS model to evaluate commerce orders'),
+      '#size' =>100,
+      '#prefix' => '<div id="' . $wrapper_id . '">',
+      '#suffix' => '</div>',
+    ];
+
+    $form['commerce_fraud_aws_model']['api_status'] = [
+      '#type' => 'radios',
+      '#title' => $this
+        ->t('Poll status'),
+      '#default_value' => $enable,
+      '#options' => [
+        1 => $this
+          ->t('Enable'),
+        0 => $this
+          ->t('Disable'),
+      ],
+       '#ajax' => [
+        'callback' => [get_class(), 'ajaxRefresh'],
+        'wrapper' => $wrapper_id,
+      ],
+    ];
+
+    if ($enable == 1) {
+
+      $api = \Drupal::state()->get('commerce_fraud_aws_api', '');
+
+      $form['commerce_fraud_aws_model']['commerce_fraud_api'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('AWS Api of Fraud Model'),
+        '#required' => TRUE,
+        '#default_value' => $api,
+      ];
+
+      $order_format = t("   [
+        'commerce_order' => [
+            'order_id' => number,
+             'user_id' => number,
+             'total_price' => commerce_price,
+             'total_quantity' => number,
+             'hostname' => number,
+             'address' => [
+                'address_line_1' => string,
+                'country_code' => string,
+                'locality' => string,
+                ]
+            'products' => [
+                'product_id' => number,
+                'product_name' => string,
+                'product_price' => commerce_price,
+                ]
+            ]
+      ]");
+
+      $form['commerce_fraud_aws_model']['orders_format'] = [
+        '#type' => 'textarea',
+        '#title' => $this->t('Format of Order details send'),
+        '#default_value' => $order_format,
+        '#disabled' => TRUE,
+        '#rows' => 19,
+      ];
+
+      $receive_format = t("   [
+        'commerce_fraud' => [
+            'fraud_count' => number,
+            'apply' => bool,
+          ]
+      ]");
+
+      $form['commerce_fraud_aws_model']['receive_format'] = [
+        '#type' => 'textarea',
+        '#title' => $this->t('Format of Post details received'),
+        '#default_value' => $receive_format,
+        '#disabled' => TRUE,
+        '#rows' => 6,
+      ];
+    }
 
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Ajax callback.
+   */
+  public static function ajaxRefresh(array $form, FormStateInterface $form_state) {
+    return $form;
   }
 
   /**
@@ -79,6 +172,25 @@ class CommerceFraudSettingsForm extends ConfigFormBase {
     $blockListValue = $form_state->getValue('commerce_fraud_blocklist_cap');
     if ($checkListValue >= $blockListValue) {
       $form_state->setErrorByName('commerce_fraud_caps][commerce_fraud_checklist_cap', t('Check List value cannot be equal to or more than Block List value'));
+    }
+    if ($form_state->getValue('api_status') == 1) {
+        if (!\Drupal::service('module_handler')->moduleExists('key')) {
+            $form_state->setErrorByName('commerce_fraud_model', t('Key Module needs to be enabled to keep the api safe'));     
+        }
+    }
+
+    if ($form_state->getValue('commerce_fraud_api')) {
+      if (!filter_var($url, FILTER_VALIDATE_URL)) {
+        $form_state->setErrorByName('commerce_fraud_api', t('Api is not a valid URL'));
+      }
+
+      if (!str_contains($url, 'api')) {
+        $form_state->setErrorByName('commerce_fraud_api', t('Api is not a valid URL'));
+      }
+
+      if (!str_contains($url, 'aws')) {
+        $form_state->setErrorByName('commerce_fraud_api', t('Api is not a valid URL'));
+      }
     }
   }
 
@@ -91,6 +203,14 @@ class CommerceFraudSettingsForm extends ConfigFormBase {
     \Drupal::state()->set('commerce_fraud_checklist_cap', $form_state->getValue('commerce_fraud_checklist_cap'));
     \Drupal::state()->set('stop_order', $form_state->getValue('stop_order'));
     \Drupal::state()->set('send_email', $form_state->getValue('send_email'));
+    \Drupal::state()->set('commerce_fraud_aws_model', (bool) $form_state->getValue('api_status')) ;
+    if (\Drupal::state()->get('commerce_fraud_model') == TRUE) {
+      $key = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+      $nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+        $ciphertext = sodium_crypto_secretbox($form_state->getValue('commerce_fraud_api'), $nonce, $key);
+      $encoded = base64_encode($nonce . $ciphertext);
+      \Drupal::state()->set('commerce_fraud_aws_api', $encoded) ;
+    }
     parent::submitForm($form, $form_state);
   }
 
